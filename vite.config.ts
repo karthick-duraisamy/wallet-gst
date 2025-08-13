9b21ef7ff6b68068f795fb916516816ba7e85a2b
Thilagavathi.D <thilagavathi.d@infinitisoftware.net>
2025-08-13 11:18:35 +0530

    design

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,17 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 3000, // 👈 change the port here
+  },
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
+  css: {
+  preprocessorOptions: {
+    scss: {
+      additionalData: `@use "src/styles/common" as *;`
+    }
+  }
+}
 });
